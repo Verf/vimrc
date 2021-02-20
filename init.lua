b5c29ec3847b237ae8953aa6a8acaeade1aaf39d
Verf <verf@protonmail.com>
2021-02-20 21:56:30 +0800

    init.vim to init.lua

---
+++ init.lua
@@ -0,0 +1,212 @@
+--  __      _______ __  __ _____   _____
+--   \ \    / /_   _|  \/  |  __ \ / ____|
+--    \ \  / /  | | | \  / | |__) | |
+--     \ \/ /   | | | |\/| |  _  /| |
+--      \  /   _| |_| |  | | | \ \| |____
+--       \/   |_____|_|  |_|_|  \_\\_____|
+--
+--  For Verf
+
+----------- Variable -------------
+local cmd, fn, g = vim.cmd, vim.fn, vim.g
+local scopes = { o = vim.o, b = vim.bo, w = vim.wo }
+
+----------- Function --------------
+local function opt(scope, key, value)
+    scopes[scope][key] = value
+    if scope ~= 'o' then scopes['o'][key] = value end
+end
+
+----------  Plugin ----------
+cmd 'packadd paq-nvim'
+local paq = require'paq-nvim'.paq
+paq{'savq/paq-nvim', opt=true}
+-- Fuzzy Search
+paq 'Yggdroot/LeaderF'
+paq 'liuchengxu/vista.vim'
+paq 'easymotion/vim-easymotion'
+-- Completion
+paq 'hrsh7th/nvim-compe'
+paq 'glepnir/lspsaga.nvim'
+paq 'neovim/nvim-lspconfig'
+-- Edit Enchance
+paq 'SirVer/ultisnips'
+paq 'sbdchd/neoformat'
+paq 'cohama/lexima.vim'
+paq 'Verf/vim-surround'
+paq 'b3nj5m1n/kommentary'
+paq 'mg979/vim-visual-multi'
+paq 'junegunn/vim-easy-align'
+-- UI Plugin
+paq 'joshdick/onedark.vim'
+paq 'glepnir/galaxyline.nvim'
+paq 'akinsho/nvim-bufferline.lua'
+paq 'kyazdani42/nvim-web-devicons'
+-- Others
+paq 'ervandew/supertab'
+paq '907th/vim-auto-save'
+paq 'airblade/vim-rooter'
+paq 'voldikss/vim-floaterm'
+paq 'simnalamburt/vim-mundo'
+paq 'farmergreg/vim-lastplace'
+-- Language
+paq 'plasticboy/vim-markdown'
+
+----------- Command -------------
+cmd 'filetype plugin indent on'
+cmd 'language en_US'
+cmd 'colorscheme onedark'
+cmd 'nohlsearch'
+cmd 'command! Bd :bp | :sp | :bn | :bd'
+
+---------- Config ----------
+opt('o', 'number',        true)
+opt('o', 'hidden',        true)
+opt('o', 'showmatch',     true)
+opt('o', 'smartcase',     true)
+opt('o', 'autochdir',     true)
+opt('o', 'termguicolors', true)
+opt('o', 'backup',        false)
+opt('o', 'showmode',      false)
+opt('o', 'hlsearch',      false)
+opt('o', 'writebackup',   false)
+opt('b', 'expandtab',     true)
+opt('b', 'smartindent',   true)
+opt('w', 'linebreak',     true)
+opt('w', 'cursorline',    true)
+
+opt('o', 'mouse',         'a')
+opt('o', 'showbreak',     '⮎')
+opt('o', 'updatetime',    300)
+opt('o', 'scrolloff',     999)
+opt('o', 'timeoutlen',    1500)
+opt('o', 'updatecount',   100)
+opt('o', 'showtabline',   2)
+opt('b', 'tabstop',       4)
+opt('b', 'softtabstop',   4)
+opt('b', 'shiftwidth',    4)
+opt('b', 'textwidth',     120)
+opt('b', 'synmaxcol',     200)
+opt('w', 'signcolumn',    'yes')
+
+opt('o', 'shortmess',     'filnxtToOFc')
+opt('o', 'switchbuf',     'useopen,usetab,newtab')
+opt('o', 'completeopt',   'menu,menuone,noselect')
+
+----------- Mappings --------------
+g['mapleader'] = ' '
+g['maplocalleader'] = ','
+require('configs.keymaps')
+
+---------- Lua Plugins --------------
+require('configs.devicons')
+require('configs.galaxyline')
+require('configs.bufferline')
+require('configs.lsp')
+require('configs.completion')
+
+---------- Vim Plugins --------------
+-- easy motion
+g['EasyMotion_smartcase'] = 1
+g['EasyMotion_do_mapping'] = 0
+
+-- easy-align
+g['easy_align_delimiters'] = {
+    ['-'] = { pattern = '--\\+',  delimiter_align = 'l', ignore_groups = {'!Comment'}},
+}
+
+-- vim-visual-multi
+g['VM_maps'] = {
+    ['Find Under']         = '<C-s>',
+    ['Find Subword Under'] = '<C-s>',
+    ['Add Cursor Down']    = '<M-Down>',
+    ['Add Cursor Up']      = '<M-Up>',
+    ['Find Next']          = '<C-s>',
+    ['Find Prev']          = '<C-S-s>',
+    ['Goto Next']          = ']',
+    ['Goto Prev']          = '[',
+    ['Seek Next']          = '<C-f>',
+    ['Seek Prev']          = '<C-b>',
+    ['Skip Region']        = '<C-x>',
+    ['Remove Region']      = '<C-S-x>',
+    ['Replace']            = 'R',
+    ['Surround']           = 'S',
+    ['Toggle Multiline']   = 'M',
+}
+
+-- vim-autosave
+g['auto_save'] = 1
+g['auto_save_silent'] = 0
+
+-- Mundo
+g['mundo_mappings'] = {
+    ["<CR>"] = 'preview',
+    n        = 'move_older',
+    i        = 'move_newer',
+    N        = 'move_older_write',
+    I        = 'move_newer_write',
+    gg       = 'move_top',
+    G        = 'move_bottom',
+    P        = 'play_to',
+    d        = 'diff',
+    l        = 'toggle_inline',
+    ["/"]    = 'search',
+    m        = 'next_match',
+    M        = 'previous_match',
+    p        = 'diff_current_buffer',
+    f        = 'diff',
+    ["?"]    = 'toggle_help',
+    q        = 'quit'
+}
+
+-- floaterm
+g['floaterm_weight']      = 0.8
+g['floaterm_height']      = 0.8
+g['floaterm_autoclose']   = 1
+g['floaterm_shell']       = 'powershell'
+g['floaterm_rootmarkers'] =  {'.project', '.git', '.gitignore', 'pom.xml', 'setup.py'}
+
+-- vim-markdown
+cmd 'au FileType markdown map <Plug> <Plug>Markdown_MoveToParentHeader'
+cmd 'au FileType markdown map <C-Enter> <Plug>Markdown_MoveToParentHeader'
+g['vim_markdown_folding_disabled'] = 1
+
+-- vim-rooter
+g['rooter_silent_chdir'] = 1
+g['rooter_patterns'] = {'.git', '.gitignore', '.project', 'pom.xml', 'setup.py'}
+
+-- vista
+g['vista_stay_on_open'] = 0
+
+-- supertab
+g['SuperTabDefaultCompletionType'] = 'context'
+g['SuperTabContextDefaultCompletionType'] = '<C-n>'
+
+-- ultisnips
+g['UltiSnipsExpandTrigger']       = '<Tab>'
+g['UltiSnipsJumpForwardTrigger']  = '<Tab>'
+g['UltiSnipsJumpBackwardTrigger'] = '<S-Tab>'
+g['UltiSnipsEditSplit'] = 'vertical'
+g['UltiSnipsSnippetStorageDirectoryForUltiSnipsEdit'] = 'ultisnips'
+
+-- leaderf
+g['Lf_ShortcutF'] = '<leader>ff'
+g['Lf_ShortcutB'] = '<leader>bb'
+g['Lf_WindowHeight'] = 0.30
+g['Lf_StlColorscheme'] = 'one'
+g['Lf_PopupColorscheme'] = 'one'
+g['Lf_WindowPosition'] = 'popup'
+g['Lf_DefaultExternalTool'] = 'rg'
+g['Lf_IgnoreCurrentBufferName'] = 1
+g['Lf_RootMarkers'] = {'.git', '.gitignore', '.project', 'pom.xml', 'setup.py'}
+g['Lf_StlSeparator'] = { left= '', right = '', font = '更纱黑体 Mono SC Nerd' }
+g['Lf_WildIgnore'] = {
+    dir = {'.git', '.vscode', '.cache', '.vim'},
+    file = {'*.sw?','~$*','*.bak','*.exe','*.o','*.so','*.py[co]'}
+}
+g['Lf_CommandMap'] = {
+    ['<C-j>'] = {'<C-n>'},
+    ['<C-k>'] = {'<C-i>'},
+    ['<C-]>'] = {'<C-->'},
+    ['<C-x>'] = {'<C-|>'},
+}
---
+++ lua/configs/keymaps.lua
@@ -0,0 +1,125 @@
+----------- Functions ---------------
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true, silent = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+----------- Mappings ----------------
+-- Norman Keyboard Layout
+map('', 'd', 'e')
+map('', 'f', 'r')
+map('', 'k', 't')
+map('', 'j', 'y')
+map('', 'r', 'i')
+map('', 'l', 'o')
+map('', 'h', 'p')
+map('', 'e', 'd')
+map('', 't', 'f')
+map('', 'g', 'g')
+map('', 'y', 'h')
+map('', 'n', 'j')
+map('', 'i', 'k')
+map('', 'o', 'l')
+map('', 'p', 'n')
+map('', 'D', 'E')
+map('', 'F', 'R')
+map('', 'K', 'T')
+map('', 'J', 'Y')
+map('', 'R', 'I')
+map('', 'L', 'O')
+map('', 'H', 'P')
+map('', 'E', 'D')
+map('', 'T', 'F')
+map('', 'Y', 'H')
+map('', 'N', 'J')
+map('', 'I', 'K')
+map('', 'O', 'L')
+map('', 'P', 'N')
+
+-- Easymotion
+map('n', 's', '<Plug>(easymotion-s2)')
+map('n', '/', '<Plug>(easymotion-sn)', {noremap=false})
+map('o', '/', '<Plug>(easymotion-tn)', {noremap=false})
+map('n', 'gl', '<Plug>(easymotion-overwin-line)')
+map('n', 'gw', '<Plug>(easymotion-bd-w)')
+map('n', 'ge', '<Plug>(easymotion-bd-e)')
+map('n', 'gf', '<Plug>(easymotion-lineforward)')
+map('n', 'gb', '<Plug>(easymotion-linebackward)')
+map('n', 'gn', '<Plug>(easymotion-j)')
+map('n', 'gi', '<Plug>(easymotion-k)')
+
+-- Easy-Align
+map('x', 'ga', '<Plug>(EasyAlign)', {noremap=false})
+map('n', 'ga', '<Plug>(EasyAlign)', {noremap=false})
+
+-- Terminal
+map('n', '<C-`>', ':FloatermToggle<CR>')
+map('t', '<C-o>', '<C-\\><C-n>')
+map('t', '<C-`>', '<C-\\><C-n>:FloatermToggle<CR>')
+map('t', '<C-t>', '<C-\\><C-n>:FloatermNew<CR>')
+map('t', '<C-q>', '<C-\\><C-n>:FloatermKill<CR>')
+map('t', '<C-n>', '<C-\\><C-n>:FloatermNext<CR>')
+map('t', '<C-p>', '<C-\\><C-n>:FloatermPrev<CR>')
+
+-- LSP
+map('n', '[e', '<cmd>lua require\'lspsaga.diagnostic\'.lsp_jump_diagnostic_prev()<CR>')
+map('n', ']e', '<cmd>lua require\'lspsaga.diagnostic\'.lsp_jump_diagnostic_next()<CR>')
+
+-- Quick
+map('n', '<leader><leader>', ':BufferLinePick<CR>')
+map('n', '<leader><Tab>', ':e#<CR>')
+
+-- Application
+map('n', '<leader>aw', ':e <CR>')
+map('n', '<leader>aa', '<CMD>lua require(\'lspsaga.codeaction\').code_action()<CR>')
+
+-- Find and Search
+map('n', '<leader>fa', ':LeaderfFile $HOME<CR>')
+map('n', '<leader>ft', ':LeaderfBufTag<CR>')
+map('n', '<leader>fh', ':Leaderf mru<CR>')
+map('n', '<leader>fp', ':<C-R>=printf(\'Leaderf rg -e %s \', expand(\'<cword>\'))<CR><CR>')
+map('n', '<leader>fw', ':<C-R>=printf(\'Leaderf rg -e \')<CR>')
+map('n', '<leader>fl', '<CMD>lua require\'lspsaga.provider\'.lsp_finder()<CR>')
+
+-- Goto
+map('n', '<leader>gd', '<CMD>lua vim.lsp.buf.definition()<CR>')
+map('n', '<leader>gi', '<CMD>lua vim.lsp.buf.implementation()<CR>')
+map('n', '<leader>gr', '<CMD>lua vim.lsp.buf.references()<CR>')
+
+-- Quit
+map('n', '<leader>qq', ':Bd<CR><CR>')
+map('n', '<leader>qw', ':qw<CR>')
+map('n', '<leader>qa', ':qa<CR>')
+map('n', '<leader>qx', ':qa!<CR>')
+
+-- rename and format
+map('n', '<leader>rn', '<CMD>lua require(\'lspsaga.rename\').rename()<CR>')
+map('n', '<leader>rm', '<CMD>lua vim.lsp.buf.formatting()<CR>')
+
+-- Mundo
+map('n','<leader>u', ':MundoToggle<CR>')
+
+-- open vimrc
+map('n', '<leader>vo', ':e $MYVIMRC<CR>')
+
+-- Tags
+map('n', '<leader>t', ':Vista!!<CR>')
+
+-- Windows Operate
+map('n', '<leader>wh', '<C-w>s')
+map('n', '<leader>wv', '<C-w>v')
+map('n', '<leader>wq', '<C-w>c')
+map('n', '<leader>wt', '<C-w>T')
+map('n', '<leader>wy', '<C-w>h')
+map('n', '<leader>wn', '<C-w>j')
+map('n', '<leader>wi', '<C-w>k')
+map('n', '<leader>wo', '<C-w>l')
+map('n', '<leader>wY', '<C-w>H')
+map('n', '<leader>wN', '<C-w>J')
+map('n', '<leader>wI', '<C-w>K')
+map('n', '<leader>wO', '<C-w>L')
+map('n', '<leader>w+', '<C-w>+')
+map('n', '<leader>w-', '<C-w>-')
+map('n', '<leader>wc', ':only<CR>')
+
