fd32846e47c1e5d88e6292c1c7d7689beb496181
Verf <verf@protonmail.com>
2021-03-11 09:02:32 +0800

    fix lualine problem

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -1,24 +1,10 @@
 require('lualine').status{
     options = {
         theme = 'onedark',
-        section_separators = {'', ''},
-        component_separators = {'', ''},
+        component_separators = nil,
         icons_enabled = true,
     },
     sections = {
-        lualine_a = { {'mode', upper = true} },
-        lualine_b = { {'branch', icon = ''} },
         lualine_c = { {'filename', file_status = true} },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location'  },
-    },
-    inactive_sections = {
-        lualine_a = {  },
-        lualine_b = {  },
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {  },
-        lualine_z = {  }
     },
 }
