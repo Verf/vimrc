03157d9a19f28f39db732782851b041343b90f6b
Verf <verf@protonmail.com>
2021-04-02 17:35:28 +0800

    small fix

--- lua/configs/compe.lua
+++ lua/configs/compe.lua
@@ -16,6 +16,10 @@ require'compe'.setup{
     }
 }
 
-map('i', '<CR>',    [[pumvisible() ? "compe#confirm(lexima#expand("<LT>CR>", "i"))" : "<CR>"]])
-map('i', '<Tab>',   [[vsnip#available(1) ? "<Plug>(vsnip-expand)" : pumvisible() ? "\<C-n>" : vsnip#jumpable(1) ? "<Plug>(vsnip-jump-next)" : "<Tab>"]], {noremap=false, expr=true})
-map('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : vsnip#jumpable(-1) ? "<Plug>(vsnip-jump-prev)" : "<S-Tab>"]], {noremap=false, expr=true})
+map('i', '<CR>',    [[pumvisible() ? "compe#confirm(lexima#expand("<LT>CR>", "i"))" : "<CR>"]], {noremap=false, expr=true})
+map('i', '<Tab>',   [[vsnip#available(1) ? "<Plug>(vsnip-expand)" : pumvisible() ? "\<C-n>" : "<Tab>"]], {noremap=false, expr=true})
+map('s', '<Tab>',   [[vsnip#available(1) ? "<Plug>(vsnip-expand)" : pumvisible() ? "\<C-n>" : "<Tab>"]], {noremap=false, expr=true})
+map('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "<S-Tab>"]], {noremap=false, expr=true})
+map('s', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "<S-Tab>"]], {noremap=false, expr=true})
+map('i', '<C-d>', [[<Plug>(vsnip-jump-next)]], {noremap=false})
+map('i', '<C-u>', [[<Plug>(vsnip-jump-prev)]], {noremap=false})
