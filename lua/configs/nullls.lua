c18f7b65afe1399258f7cd7fb66d16f0b9646dd9
Verf <verf@prtonmail.com>
2022-12-13 16:24:22 +0800

    remove unused linter

--- lua/configs/nullls.lua
+++ lua/configs/nullls.lua
@@ -1,11 +1,6 @@
 local nls = require 'null-ls'
 nls.setup {
     sources = {
-        -- code actions
-        nls.builtins.code_actions.eslint.with {
-            prefer_local = true,
-        },
-        -- formatting
         nls.builtins.formatting.blue,
         nls.builtins.formatting.isort,
         nls.builtins.formatting.shfmt,
@@ -14,10 +9,5 @@ nls.setup {
         },
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.sql_formatter,
-        -- linter
-        nls.builtins.diagnostics.eslint.with {
-            prefer_local = true,
-        },
-        -- nls.builtins.diagnostics.pylint,
     },
 }
