e28aa79cc3b721106c0c9a959d84d76f0519d3b8
Verf <verf@prtonmail.com>
2023-07-21 10:04:02 +0800

    change some settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -9,53 +9,26 @@ cmd [[command! Editrc :e $MYVIMRC]]
 g.mapleader = ' '
 g.maplocalleader = ','
 
-cmd [[autocmd FileType netrw setlocal bufhidden=wipe]]
-cmd [[autocmd FileType html setlocal shiftwidth=2 tabstop=2]]
-cmd [[autocmd FileType css setlocal shiftwidth=2 tabstop=2]]
-cmd [[autocmd FileType javascript setlocal shiftwidth=2 tabstop=2]]
-cmd [[autocmd FileType vue setlocal shiftwidth=2 tabstop=2]]
-cmd [[autocmd FileType json setlocal shiftwidth=2 tabstop=2]]
-cmd [[autocmd FileType markdown setlocal shiftwidth=2 tabstop=2]]
-
--- autocmd
-vim.api.nvim_create_autocmd({ 'InsertLeave', 'BufLeave', 'FocusLost' }, {
-    callback = function()
-        if vim.bo.modified and not vim.bo.readonly and vim.fn.expand '%' ~= '' and vim.bo.buftype == '' then
-            vim.api.nvim_command 'silent update'
-            vim.api.nvim_command [[ echo strftime('%X', localtime()) "autosave"]]
-        end
-    end,
-})
-
 ----- settings -----
 opt.wrap = false
-opt.hidden = true
 opt.showmatch = true
 opt.ignorecase = true
 opt.smartcase = true
 opt.autochdir = false
 opt.termguicolors = true
 opt.foldenable = false
-opt.backup = false
-opt.undofile = true
 opt.showmode = false
 opt.hlsearch = false
-opt.writebackup = false
 opt.expandtab = true
-opt.autoindent = true
 opt.smartindent = true
 opt.number = true
 opt.relativenumber = true
-opt.linebreak = true
 opt.cursorline = true
-opt.lazyredraw = true
 opt.shellslash = false
 opt.spell = false
-opt.autoread = true
 opt.autowrite = true
-opt.spelllang = { 'en_us' }
 
-opt.updatetime = 1000
+opt.updatetime = 750
 opt.scrolloff = 999
 opt.timeoutlen = 1500
 opt.updatecount = 100
@@ -64,27 +37,18 @@ opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.synmaxcol = 200
-opt.winblend = 10
-
-opt.foldcolumn = '1'
-opt.foldlevel = 99
-opt.foldlevelstart = 99
 
 opt.mouse = 'a'
 opt.showbreak = '⮎'
-opt.signcolumn = 'yes'
 opt.virtualedit = 'all'
 opt.fileformats = 'unix,dos'
 opt.clipboard = 'unnamed'
 opt.shell = 'pwsh -nol'
-opt.guifont = 'FiraCode NF:h13'
 opt.shellcmdflag =
     '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
 opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-opt.shellquote = ''
 opt.shellxquote = ''
-opt.shortmess = 'filnxtToOFc'
 opt.whichwrap = ''
 opt.spelllang = { 'en_us' }
 opt.switchbuf = { 'useopen', 'usetab', 'newtab' }
