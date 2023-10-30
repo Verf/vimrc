81a50b881f743f9abc41e953b5fe4b08aed161bf
Verf <verf@protonmail.com>
2023-10-30 08:44:58 +0800

    disable lsp file watch to prevent neovim#23291

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -854,7 +854,12 @@ local plugins = {
             }
 
             -- capabilities
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = vim.tbl_deep_extend(
+                'force',
+                vim.lsp.protocol.make_client_capabilities(),
+                require('cmp_nvim_lsp').default_capabilities()
+            )
+            capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
 
             require('lspconfig').pyright.setup {
                 capabilities = capabilities,
