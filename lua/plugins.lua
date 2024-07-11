d5dbbf7f5f6a94063821c0376069957108c7f9f8
Verf <verf@protonmail.com>
2024-07-11 09:05:18 +0800

    add sub plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -307,6 +307,7 @@ require('nvim-treesitter.configs').setup {
         'python',
         'javascript',
         'vimdoc',
+        'regex',
     },
     highlight = {
         enable = true,
@@ -372,3 +373,22 @@ require('conform').setup {
     },
 }
 vim.keymap.set('n', '<leader>m', '<CMD>lua require("conform").format({async=true})<CR>', { desc = 'Format' })
+
+add { source = 'chrisgrieser/nvim-rip-substitute' }
+require('rip-substitute').setup {
+    keymaps = {
+        -- normal & visual mode
+        confirm = '<CR>',
+        abort = 'q',
+        prevSubst = '<C-p>',
+        nextSubst = '<C-n>',
+        openAtRegex101 = 'R',
+        insertModeConfirm = '<C-CR>', -- (except this one, obviously)
+    },
+}
+vim.keymap.set(
+    { 'n', 'x' },
+    '<leader>s',
+    function() require('rip-substitute').sub() end,
+    { desc = ' rip substitute' }
+)
