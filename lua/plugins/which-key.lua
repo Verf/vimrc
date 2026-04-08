cab71b8f573fe87e5bd28ef6a5d171c77a2a74d5
Verf <verf@protonmail.com>
2026-04-09 00:14:35 +0800

    adjust which key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,6 +2,9 @@ return {
     'folke/which-key.nvim',
     event = 'VeryLazy',
     opts = {
+        preset = 'modern',
+        delay = 250,
         icons = { mappings = false },
+        triggers = { { '<auto>', mode = 'nso' } },
     },
 }
