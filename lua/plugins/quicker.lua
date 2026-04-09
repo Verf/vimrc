c5d42e8eff405c3f8f46eb7d3662b6240ca0ad50
Verf <verf@protonmail.com>
2026-04-09 21:42:02 +0800

    remove quicker and toggleterm

--- lua/plugins/quicker.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'stevearc/quicker.nvim',
-  ft = "qf",
-  keys = {
-      {'<leader>c', [[<cmd>lua require("quicker").toggle()<cr>]], mode = 'n', desc = 'Quicker'},
-  },
-  opts = {},
-}
