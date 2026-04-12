f1bcea89a99e5a56d1553b6072b610194722b683
Verf <verf@protonmail.com>
2026-04-12 13:20:10 +0800

    adjust overseer for python_run

--- lua/overseer/template/python_run.lua
+++ lua/overseer/template/python_run.lua
@@ -1,14 +1,15 @@
 return {
-  name = "Run Current buffer by Python",
-  builder = function()
-    return {
-      cmd = { "python" },
-      args = { vim.fn.expand("%:p") },
-      cwd = vim.fn.getcwd(),
-      components = { "default" },
-    }
-  end,
-  condition = {
-    filetype = { "python" },
-  },
+    name = 'Run Current buffer by Python',
+    builder = function()
+        return {
+            cmd = { 'python' },
+            args = { vim.fn.expand '%:p' },
+            cwd = vim.fn.getcwd(),
+            name = 'Python: ' .. vim.fn.expand '%:t',
+            components = { 'default', { 'on_complete_notify' }, { 'display_duration', detail_level = 2 } },
+        }
+    end,
+    condition = {
+        filetype = { 'python' },
+    },
 }
--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -4,11 +4,9 @@ return {
     keys = {
         { '<leader>or', '<cmd>OverseerRun<cr>', desc = 'Run Task' },
         { '<leader>ot', '<cmd>OverseerToggle<cr>', desc = 'Toggle Task List' },
-        { '<leader>oa', '<cmd>OverseerQuickAction<cr>', desc = 'Task Quick Action' },
-        { '<leader>ol', '<cmd>OverseerLoadBundle<cr>', desc = 'Load Task Bundle' },
     },
     opts = {
-        strategy = 'terminal', -- 推荐：用内置 terminal（干净、支持 Quickfix）
+        strategy = 'terminal',
         -- VSCode-like
         component_aliases = {
             default = {
