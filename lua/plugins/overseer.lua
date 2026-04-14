dc3263c398932d35b49fa312cee2ddf2e3f2b48c
Verf <verf@protonmail.com>
2026-04-14 17:16:09 +0800

    add overseer restart last cmd

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -4,6 +4,7 @@ return {
     keys = {
         { '<leader>or', '<cmd>OverseerRun<cr>', desc = 'Run Task' },
         { '<leader>ot', '<cmd>OverseerToggle<cr>', desc = 'Toggle Task List' },
+        { '<leader>ol', '<cmd>OverseerRestartLast<cr>', desc = 'Restart Last Task' },
     },
     config = function()
         require('overseer').setup {
@@ -27,5 +28,24 @@ return {
                 },
             },
         }
+
+        vim.api.nvim_create_user_command('OverseerRestartLast', function()
+            local overseer = require 'overseer'
+            local task_list = require 'overseer.task_list'
+            local tasks = overseer.list_tasks {
+                status = {
+                    overseer.STATUS.SUCCESS,
+                    overseer.STATUS.FAILURE,
+                    overseer.STATUS.CANCELED,
+                },
+                sort = task_list.sort_finished_recently,
+            }
+            if vim.tbl_isempty(tasks) then
+                vim.notify('No tasks found', vim.log.levels.WARN)
+            else
+                local most_recent = tasks[1]
+                overseer.run_action(most_recent, 'restart')
+            end
+        end, {})
     end,
 }
