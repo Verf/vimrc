f282f3081b19f3b563546d7e0370766d59dc26ad
Verf <verf@protonmail.com>
2026-04-16 11:53:29 +0800

    remove ression auto save/load

--- lua/plugins/resession.lua
+++ lua/plugins/resession.lua
@@ -2,9 +2,24 @@ return {
     'stevearc/resession.nvim',
     lazy = false,
     keys = {
-        { '<leader>ss', function() require('resession').save() end, mode = 'n', desc = 'Session Save' },
-        { '<leader>sl', function() require('resession').load() end, mode = 'n', desc = 'Session Load' },
-        { '<leader>sd', function() require('resession').delete() end, mode = 'n', desc = 'Session Delete' },
+        {
+            '<leader>ss',
+            function() require('resession').save_tab(vim.fn.getcwd(), { dir = 'dirsession', notify = true }) end,
+            mode = 'n',
+            desc = 'Session Save',
+        },
+        {
+            '<leader>sl',
+            function() require('resession').load(vim.fn.getcwd(), { dir = 'dirsession', silence_errors = true }) end,
+            mode = 'n',
+            desc = 'Session Load',
+        },
+        {
+            '<leader>sd',
+            function() require('resession').delete(vim.fn.getcwd(), { dir = 'dirsession', notify = true }) end,
+            mode = 'n',
+            desc = 'Session Delete',
+        },
     },
     opts = {
         -- 为scope.nvim配置过滤器
@@ -18,29 +33,4 @@ return {
         -- 添加scope.nvim扩展
         extensions = { scope = {}, overseer = {} },
     },
-    config = function(_, opts)
-        local resession = require 'resession'
-        resession.setup(opts)
-
-        local rs_group = vim.api.nvim_create_augroup('MyResessionGroup', { clear = true })
-        vim.api.nvim_create_autocmd('VimEnter', {
-            group = rs_group,
-            callback = function()
-                -- 仅当neovim不加参数或stdin输入启动时恢复session
-                if vim.fn.argc(-1) == 0 and not vim.g.using_stdin then
-                    -- 将session存储在dirsession目录下
-                    resession.load(vim.fn.getcwd(), { dir = 'dirsession', silence_errors = true })
-                end
-            end,
-            nested = true,
-        })
-        vim.api.nvim_create_autocmd('VimLeavePre', {
-            group = rs_group,
-            callback = function() resession.save_tab(vim.fn.getcwd(), { dir = 'dirsession', notify = false }) end,
-        })
-        vim.api.nvim_create_autocmd('StdinReadPre', {
-            group = rs_group,
-            callback = function() vim.g.using_stdin = true end,
-        })
-    end,
 }
