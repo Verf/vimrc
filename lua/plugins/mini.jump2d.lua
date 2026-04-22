65f62bbf1feaeeb29969806bd6075221afab7380
Verf <verf@protonmail.com>
2026-04-22 19:04:09 +0800

    adjust mini.jump2d and mini.pick

--- lua/plugins/mini.jump2d.lua
+++ lua/plugins/mini.jump2d.lua
@@ -5,7 +5,7 @@ return {
     keys = {
         {
             'gw',
-            function() MiniJump2d.start(MiniJump2d.builtin_opts.word_start) end,
+            function() MiniJump2d.start { spotter = MiniJump2d.gen_spotter.pattern '%f[%w]%w+' } end,
             mode = { 'n', 'x', 'o' },
             desc = 'Goto words',
         },
@@ -13,7 +13,7 @@ return {
             'gs',
             function() MiniJump2d.start { spotter = MiniJump2d.gen_spotter.pattern '%p+' } end,
             mode = { 'n', 'x', 'o' },
-            desc = 'Goto next symbols',
+            desc = 'Goto symbols',
         },
         {
             'gl',
@@ -21,7 +21,6 @@ return {
                 MiniJump2d.start {
                     spotter = MiniJump2d.builtin_opts.line_start.spotter,
                     hooks = { after_jump = MiniJump2d.builtin_opts.line_start.hooks.after_jump },
-                    view = { n_steps_ahead = 2 },
                 }
             end,
             mode = { 'n', 'x', 'o' },
@@ -32,6 +31,7 @@ return {
         -- mini.jump2d永远以labels中字母的顺序，按屏幕从上到下，从左到右为spot标记label。
         -- 考虑到一般来说希望跳转过去的位置都是较远的，因此这里设置开头和结尾的字母为更加顺手的字母。
         labels = 'tesfdwvcxqazkgbjyplhmruoin',
+        view = { n_steps_ahead = 99 },
         allowed_windows = { not_current = false },
         mappings = { start_jumping = '' },
     },
