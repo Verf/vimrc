6a91e4d171d9786a0ae5cf2562f603a40041869e
Verf <verf@protonmail.com>
2026-04-23 18:50:20 +0800

    adjust mini.jump2d labels and dim

--- lua/plugins/mini.jump2d.lua
+++ lua/plugins/mini.jump2d.lua
@@ -28,10 +28,11 @@ return {
         },
     },
     opts = {
-        -- mini.jump2d永远以labels中字母的顺序，按屏幕从上到下，从左到右为spot标记label。
-        -- 考虑到一般来说希望跳转过去的位置都是较远的，因此这里设置开头和结尾的字母为更加顺手的字母。
-        labels = 'tesfdwvcxqazkgbjyplhmruoin',
-        view = { n_steps_ahead = 99 },
+        -- mini.jump2d永远以labels中字母的顺序，按屏幕从上到下，从左到右的顺序为spot标记label。
+        -- 根据实际体验+/-10行左右位置是最为常见进行跳转使用的位置，因此考虑设置这里为最常用按键
+        -- 同时对于qzkjbp这几个比较难以按到的键位，不安排到labels中
+        labels = 'gaxcwdtesfvoinulrmhy',
+        view = { dim = true, n_steps_ahead = 99 },
         allowed_windows = { not_current = false },
         mappings = { start_jumping = '' },
     },
