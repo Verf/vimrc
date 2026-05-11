9c614d7344def2777d5c711dee3b18b5856c67a0
Verf <verf@protonmail.com>
2026-05-11 15:03:21 +0800

    add find notes

--- lua/plugins/mini.pick.lua
+++ lua/plugins/mini.pick.lua
@@ -7,6 +7,13 @@ return {
         { '<leader>f', function() MiniPick.builtin.files() end, mode = 'n', desc = 'Files' },
         { '<leader>b', function() MiniPick.builtin.buffers() end, mode = 'n', desc = 'Buffers' },
         { '<leader>/', function() MiniPick.builtin.grep_live() end, mode = 'n', desc = 'Grep Live' },
+        -- for orgmode.nvim
+        {
+            '<leader>of',
+            function() MiniPick.builtin.files({}, { source = { cwd = vim.fn.expand(vim.env.NOTE_TAKING_DIR) } }) end,
+            mode = 'n',
+            desc = 'Find Notes',
+        },
     },
     opts = {
         options = { use_cache = true },
