16e1cadd5a69b8392db55809b5a6ed7e6f2fcdd0
Verf <verf@protonmail.com>
2026-05-12 14:41:20 +0800

    adjust hipatterns

--- lua/plugins/mini.hipatterns.lua
+++ lua/plugins/mini.hipatterns.lua
@@ -8,10 +8,6 @@ return {
         local extra = require 'mini.extra'
         hipatterns.setup {
             highlighters = {
-                fixme = extra.gen_highlighter.words({ 'FIXME' }, 'MiniHipatternsFixme'),
-                hack = extra.gen_highlighter.words({ 'HACK' }, 'MiniHipatternsHack'),
-                todo = extra.gen_highlighter.words({ 'TODO' }, 'MiniHipatternsTodo'),
-                note = extra.gen_highlighter.words({ 'NOTE' }, 'MiniHipatternsNote'),
                 hex_color = hipatterns.gen_highlighter.hex_color(),
             },
         }
