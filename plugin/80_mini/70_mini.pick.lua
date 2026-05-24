450f51909bf4743a6cd8bc39823b33c1c00c08ad
Verf <verf@protonmail.com>
2026-05-24 21:47:30 +0800

    clean find notes

--- plugin/80_mini/70_mini.pick.lua
+++ plugin/80_mini/70_mini.pick.lua
@@ -11,9 +11,3 @@ end)
 vim.keymap.set('n', '<leader>f', function() MiniPick.builtin.files() end, { desc = 'Files' })
 vim.keymap.set('n', '<leader>b', function() MiniPick.builtin.buffers() end, { desc = 'Buffers' })
 vim.keymap.set('n', '<leader>/', function() MiniPick.builtin.grep_live() end, { desc = 'Grep Live' })
-vim.keymap.set(
-    'n',
-    '<leader>of',
-    function() MiniPick.builtin.files({}, { source = { cwd = vim.fn.expand(vim.env.NOTE_TAKING_DIR) } }) end,
-    { desc = 'Find Notes' }
-)
