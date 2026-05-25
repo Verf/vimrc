1ec17c5f030d584d6cec4deb3846b1f4a7eadc75
Verf <verf@protonmail.com>
2026-05-25 08:55:06 +0800

    use oxfmt

--- plugin/90_plugins/50_conform.lua
+++ plugin/90_plugins/50_conform.lua
@@ -1,17 +1,22 @@
 vim.pack.add { 'https://github.com/stevearc/conform.nvim' }
 
-Config.later(function()
-    require('conform').setup {
-        default_format_opts = { lsp_format = 'fallback' },
-        formatters_by_ft = {
-            python = { 'ruff_format', 'ruff_organize_imports' },
-            lua = { 'stylua' },
-            json = { 'biome' },
-            nu = { 'nufmt' },
-            toml = { 'taplo' },
-            yaml = { 'yamlfmt' },
-        },
-    }
-end)
+Config.later(
+    function()
+        require('conform').setup {
+            default_format_opts = { lsp_format = 'fallback' },
+            formatters_by_ft = {
+                python = { 'ruff_format', 'ruff_organize_imports' },
+                lua = { 'stylua' },
+                nu = { 'nufmt' },
+                toml = { 'taplo' },
+                yaml = { 'yamlfmt' },
+                json = { 'oxfmt' },
+                javascript = { 'oxfmt' },
+                typescript = { 'oxfmt' },
+                vue = { 'oxfmt' },
+            },
+        }
+    end
+)
 
 vim.keymap.set('n', '<leader>m', function() require('conform').format { async = true } end, { desc = 'Format' })
