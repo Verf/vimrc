18cb43058226a6d8d9a57ae6714807a20f69e4e4
Verf <verf@protonmail.com>
2026-05-25 09:34:23 +0800

    add note keymap, update hiparttens

--- plugin/80_mini/20_mini.hipatterns.lua
+++ plugin/80_mini/20_mini.hipatterns.lua
@@ -3,6 +3,11 @@ Config.now_if_args(function()
     local hipatterns = require 'mini.hipatterns'
     hipatterns.setup {
         highlighters = {
+            fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+            hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+            note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+            todo = { pattern = '%f[%w]()TODO()%f[%W]', group = hipatterns.compute_hex_color_group('#ff0000', 'fg') },
+            done = { pattern = '%f[%w]()DONE()%f[%W]', group = hipatterns.compute_hex_color_group('#00ff00', 'fg') },
             hex_color = hipatterns.gen_highlighter.hex_color(),
         },
     }
