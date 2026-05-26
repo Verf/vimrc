122191fa82be9d163c2230d13c003a6aaca21d2a
Verf <verf@protonmail.com>
2026-05-26 15:10:04 +0800

    update markdown config

--- plugin/90_plugins/54_render_markdown.lua
+++ plugin/90_plugins/54_render_markdown.lua
@@ -1,5 +1,10 @@
 vim.pack.add { 'https://github.com/MeanderingProgrammer/render-markdown.nvim' }
 
 Config.on_filetype('markdown', function()
-    require('render-markdown').setup {}
+    require('render-markdown').setup {
+        heading = { sign = true, icons = { ' ', ' ', ' ', ' ', ' ', ' ' } },
+        code = { sign = true, width = 'block', right_pad = 1 },
+        dash = { width = 'full' },
+        checkbox = { enabled = true },
+    }
 end)
