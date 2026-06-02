5c1c88fc45b9d920661fab5bed2ba4fbca14df31
Verf <verf@protonmail.com>
2026-06-02 17:59:03 +0800

    update shapeim dict

--- plugin/90_plugins/14_shapeim.lua
+++ plugin/90_plugins/14_shapeim.lua
@@ -3,7 +3,7 @@ vim.pack.add { 'https://github.com/Verf/shapeim.nvim' }
 Config.now(
     function()
         require('shapeim').setup {
-            dict_path = '~/Syncthing/Config/Rime/wubi986.dict.yaml',
+            dict_path = '~/AppData/Roaming/Rime/wubi986n.dict.yaml',
             toggle_key = '<C-\\>',
             persist_state = true,
         }
