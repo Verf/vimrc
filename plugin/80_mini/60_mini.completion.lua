2fc08a859d63be62515ad0355b7a5f1234ac46da
Verf <verf@protonmail.com>
2026-06-05 19:26:46 +0800

    replace blink with mini.completion

---
+++ plugin/80_mini/60_mini.completion.lua
@@ -0,0 +1,12 @@
+-- mini.completion
+Config.now(
+    function()
+        require('mini.completion').setup {
+            lsp_completion = {
+                source_func = 'omnifunc',
+                auto_setup = false,
+            },
+            fallback_action = '<C-n>',
+        }
+    end
+)
---
+++ plugin/90_plugins/55_path_lsp.lua
@@ -0,0 +1,12 @@
+-- path-lsp: 文件系统路径补全 LSP
+-- 使用 Neovim 内嵌 function transport，无外部进程依赖。
+Config.now(function()
+    local path_lsp = require 'plugins.path_lsp'
+
+    -- 在所有打开的 buffer 上启动路径补全 LSP
+    vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile', 'BufWinEnter', 'BufEnter' }, {
+        group = _G.MyGroup,
+        callback = function(args) path_lsp.start(args.buf) end,
+        desc = 'Start path-lsp on buffer open',
+    })
+end)
